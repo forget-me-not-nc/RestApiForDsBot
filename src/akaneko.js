d0541deb6f61082de1d94254408c9e51cc5ca05c
forget-me-not-nc <forget.me.not.x64@gmail.com>
2021-12-30 21:03:07 +0200

    Setting Procfile

--- src/akaneko.js
+++ src/akaneko.js
@@ -1,16 +1,8 @@
 const akaneko = require('akaneko');
 
-function yourFunctionName() {
-
- // console.log("SFW Neko: " + await akaneko.neko());
-
- // console.log("Lewd Neko:" + await akaneko.lewdNeko());
-
+function yourFunctionName()
+{
     return akaneko.nsfw.hentai();
-
- // console.log("BDSM: " + await akaneko.nsfw.bdsm());
- // console.log("Maid: " + await akaneko.nsfw.maid());
-  //console.log("Hentai: " + await akaneko.nsfw.hentai());
 }
 
 module.exports.yourFunctionName = yourFunctionName;
