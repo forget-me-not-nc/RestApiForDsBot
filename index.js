6588a228cd616b81b6c58ff926c8b4908f87b180
forget-me-not-nc <forget.me.not.x64@gmail.com>
2021-12-30 20:57:58 +0200

    Initial commit

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { yourFunctionName } = require('./src/akaneko');
 const api = express(); 
 
-api.get('/image',  (req, res) => {
-
+api.get('/image',  (req, res) =>
+{
     yourFunctionName().then((response) => {
         res.send(response);
     })
-
 });
 
 api.listen(3000);
